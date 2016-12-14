0c0d5fb9357dcd966cefba2633dc702ba7495adb
fumishitan <hounen.hounen@gmail.com>
2016-12-14 23:54:12 +0900

    Commit
    Repair Key

--- File/make-news.js
+++ File/make-news.js
@@ -1,9 +1,7 @@
 // JavaScript source code
 var news_element;
-//var YOUR_APP_KEY = "YOUR_APP_KEY";
-//var YOUR_CLIENT_KEY = "YOUR_CLIENT_KEY";
-var YOUR_APP_KEY = "977ef64bffbd599d0defaba309a7c8046ebc23e49551de00881f75a1bd20eb58";
-var YOUR_CLIENT_KEY = "08aff8c825b48062595a57b6e032ac327afffe90038bf1e88b8c88ad4501613d";
+var YOUR_APP_KEY = "YOUR_APP_KEY";
+var YOUR_CLIENT_KEY = "YOUR_CLIENT_KEY";
 var ncmb;
 
 
